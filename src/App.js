13eb3c65ec5dae93e3e94b4a4edc18fe75cfcddb
Vikram Dabali <Vikramviki09@gmail.com>
2021-05-31 09:32:03 +0530

    created app header with countries dropdown functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [ countries, setCountries ] = useState([])
+  const [ country, setCountry] = useState("Worldwide")
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch(`https://disease.sh/v3/covid-19/countries`)
+      .then(response => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+        setCountries(countries);  
+      })
+    };
+
+    getCountriesData();
+  },[])
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className="app_stats">
+            
+      </div>
     </div>
   );
 }
